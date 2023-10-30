a20dd43651e9810fd5d7d57126f1e1b7a8619714
DESKTOP-M7S1RDP\Pascal Spring <pascal.spring@waidli.ch>
2023-10-30 22:25:43 +0100

    8===D

---
+++ Untitled-1.py
@@ -0,0 +1,29 @@
+class Worker():
+    def init(self, vorname :str, nachname :str, lohn :int):
+        self.vorname = vorname
+        self.nachname = nachname
+        self.lohn = lohn
+
+
+    def senken_lohn(self, Franken):
+        if (Franken <= 3600):
+            print("Error: Lohn unter dem Mindestlohn! \
+Der Wert wurde nicht geändert")
+        else:
+            self.lohn = Franken
+            print("Lohn wurde angepasst.")
+
+    def get_lohn(self):
+        return self.lohn
+
+    def get_worker_id(self):
+        return "Ich heisse " + self.vorname + self.nachname + ". Mein Lohn bei der Firma beträgt " + str(self.lohn)
+
+    def get_initialen():
+        return
+
+
+p1 = Worker("Max", "Mustermann", 3600)
+p2 = Worker(p1, "Mustermann", 3600)
+
+print(p1.get_worker_id())
